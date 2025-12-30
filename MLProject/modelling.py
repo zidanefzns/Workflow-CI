206258e898ad26fa964977fde742157344bd8786
Zidane Fauzan <zidanefzn21@gmail.com>
2025-12-30 22:21:29 +0700

    add_ CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,27 +4,17 @@ import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.impute import SimpleImputer
 
-
 # ===============================
-# Load dataset preprocessing
+# Load dataset
 # ===============================
-data_path = "PredDeposit_preprocessed.csv"
-df = pd.read_csv(data_path)
+df = pd.read_csv("PredDeposit_preprocessed.csv")
 
-# Tentukan kolom target (samakan dengan notebook)
-target_col = "deposit"   
-
+target_col = "deposit"
 X = df.drop(columns=[target_col])
-y = df[target_col]
-
-# ===============================
-# Handle missing value pada TARGET
-# ===============================
-y = y.fillna(y.mode()[0])
-
+y = df[target_col].fillna(df[target_col].mode()[0])
 
 # ===============================
 # Split data
@@ -33,30 +23,26 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Handle missing value
-from sklearn.impute import SimpleImputer
+# ===============================
+# Handle missing values
+# ===============================
 imputer = SimpleImputer(strategy="mean")
 X_train = imputer.fit_transform(X_train)
 X_test = imputer.transform(X_test)
 
 # ===============================
-# MLflow setup (LOCAL)
+# MLflow autolog
 # ===============================
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment("Deposit-Prediction-Basic")
-
 mlflow.sklearn.autolog()
 
 # ===============================
-# Training model
+# Train model
 # ===============================
 with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
-    # Evaluasi
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
     print("Accuracy:", acc)
-    print(classification_report(y_test, y_pred))
